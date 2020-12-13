997463c85f17279c78471b5e6310e726bee7fae5
本行　圭介 <kei.hongyo@gmail.com>
2020-12-13 16:20:22 +0900

    Add Comment

--- main.go
+++ main.go
@@ -26,17 +26,19 @@ func main() {
 	d.Configture()
 
 	for {
-
+		/* Get Accelerometer Data */
 		err = d.getGyro()
 		if err == nil {
 			fmt.Printf("Gyro : x=%5.2f y=%5.2f z=%5.2f\n", d.dt.xGyro, d.dt.yGyro, d.dt.zGyro)
 		}
 
+		/* Get Gyro Data */
 		err = d.getAcc()
 		if err == nil {
 			fmt.Printf("Acc  : x=%5.2f y=%5.2f z=%5.2f\n", d.dt.xAcc, d.dt.yAcc, d.dt.zAcc)
 		}
 
+		/* Get Geomagnetic sensor Data*/
 		err = d.getMag()
 		if err == nil {
 			fmt.Printf("Mag  : x=%4d y=%4d z=%4d\n", d.dt.xMag, d.dt.yMag, d.dt.zMag)
